fa54bb314ae289bcb6e76a832249fe5cd732219f
Bảo Anh Nguyễn <88286671+alicee-19@users.noreply.github.com>
2025-11-28 03:16:06 +0700

    fix: prettier & build error

--- src/features/vr-lessons/components/forms/phase-one-step-three.tsx
+++ src/features/vr-lessons/components/forms/phase-one-step-three.tsx
@@ -8,7 +8,7 @@ import { VrLessonNameInput } from '../vr-lesson-name-input';
 import type { VrLessonCreatePayload, VrTaskCreatePayload } from '@/common/types/vr-lesson.type';
 
 export function PhaseOneStepThree() {
-  const { lessonData, updateLessonData, goToStep, submitForm, tasks } = useFormContext();
+  const { lessonData, updateLessonData, goToStep, tasks } = useFormContext();
   const { formData, updateField, errors, isValid, createVrLessonPhaseOneMutate } = useStepThreeForm(
     { name: lessonData.name, description: lessonData.description },
     updateLessonData,
